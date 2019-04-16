beecbbed9981792fb193f4178307258ef3a4922a
MUNROE <319681@dadeschools.net>
2019-04-16 14:08:10 -0400

    finished html; started on js

---
+++ main.js
@@ -0,0 +1,23 @@
+var panel = $("#quiz-area");
+
+var countStartNumber = 30;
+
+// Question set
+var questions = [
+    
+    {
+        question: "What was the first full length CGI movie?",
+        answers: ["A Bug's Life", "Monsters Inc.", "Toy Story", "The Lion King"],
+        correctAnswer: "Toy Story",
+        image: "assets\Toy Story p7.gif"
+    },
+
+    {
+        question: "Which of these is NOT a name of one of the Spice Girls?",
+        answers: ["Sporty Spice", "Hot Spice", "Scary Spice", "Posh Spice"],
+        correctAnswer: "Hot Spice",
+        image: "assets\spice girls p7.gif"
+    }
+
+
+]
